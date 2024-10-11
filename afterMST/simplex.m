1caf2a6f75cf9c65c09b2d5b8be8a713af3d3a8d
som <gamchngr21@outlook.com>
2024-10-11 15:25:51 +0530

    new

--- afterMST/simplex.m
+++ afterMST/simplex.m
@@ -1,56 +1,57 @@
-clc;
+clc
 clear all
-format short;
-C = [2 5];
-a = [1 4;3 1;1 1];
+format short 
+c = [2 5];
+a = [1 4; 3 1; 1 1];
 b = [24; 21; 9];
-[m,n] = size(a);
-s = eye(m);
+[m, n] = size(a);
+s = eye(m); 
 A = [a s b];
-cost = zeros(1, n+m+1);
-cost(1:n) = C;
-bv = n+1:1:n+m;
-display(bv);
-
-zjcj = cost(bv) * A - cost;
+cost = zeros(1, n + m + 1);
+cost(1:n) = c; 
+BV = n + 1:1:n + m;
+display(BV);
+zjcj = cost(BV) * A - cost;
 zcj = [zjcj; A];
-simpletable = array2table(zcj);
-simpletable.Properties.VariableNames(1:n+m+1) = {'x_1', 'x_2', 's_1', 's_2', 's_3', 'sol'};
+simplex_table = array2table(zcj);
+simplex_table.Properties.VariableNames(1:n + m + 1) = {'x1', 'x2', 's1', 's2', 's3', 'sol'};
 flag = true;
-
 while flag
-    if any(zjcj < 0)
-        fprintf('the current BFS is not optimal \n');
+    if any(zjcj > 0) 
+        fprintf("Current BFS is not optimal\n");
         zc = zjcj(1:end-1);
-        [Enter_val, pvt_col] = min(zc);
+        [Enter_val, pvt_col] = min(zc); % Find entering variable (min value)
+        
         if all(A(:, pvt_col) <= 0)
-            error('LPP is Unbounded all enteries are <=0 in column %d', pvt_col);
+            error('LPP is unbounded; all entries are <=0 in column %d', pvt_col);
         else
-            sol = A(:, end)
-            column=A(:, pvt_col)
-            for i=1:m
+            sol = A(:, end);
+            column = A(:, pvt_col);
+            for i = 1:m
                 if column(i) > 0
-                    ratio(i) = sol(i)./column(i)
+                    ratio(i) = sol(i) ./ column(i);
                 else
-                    ratio(i) = inf
-                end
-            end
-            [leaving_val, pvt_row] = min(ratio)
+                    ratio(i) = inf;
+                end 
+            end 
+            [leaving_val, pvt_row] = min(ratio);
         end
-        bv(pvt_row) = pvt_col;
+        
+        BV(pvt_row) = pvt_col;
         pvt_key = A(pvt_row, pvt_col);
-        A(pvt_row, :) = A(pvt_row,:)./pvt_key;
-        for i=1:m
-            if i==pvt_row
-                A(i, :) = A(i, :)-A(i, pvt_col).*A(pvt_row,:);
-            end
-        end
-        zjcj = zjcj-zjcj(pvt_col).*A(pvt_row, :);
-        zcj = [zjcj;A];
+        A(pvt_row, :) = A(pvt_row, :) ./ pvt_key; 
+        for i = 1:m
+            if i ~= pvt_row
+                A(i, :) = A(i, :) - A(i, pvt_col) .* A(pvt_row, :); 
+            end 
+        end 
+        zjcj = zjcj - zjcj(pvt_col) .* A(pvt_row, :); 
+        zcj = [zjcj; A]; % Update tableau
         table = array2table(zcj);
-        table.Properties.VariableNames(1:n+m+1) = {'x_1', 'x_2', 's_1', 's_2', 's_3', 'sol'}
+        table.Properties.VariableNames(1:n + m + 1) = {'x1', 'x2', 's1', 's2', 's3', 'sol'};
     else
-        flag = false;
-        fprintf('The current BFS is optimal \n')
-    end
+        flag = false; % Optimal solution found
+        fprintf('The current BFS is optimal\n');
+    end 
 end
+
